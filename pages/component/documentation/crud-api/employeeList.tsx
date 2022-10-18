924f1e21f585709aa7e73ee393acc14cbbd1271a
Pavan Kumar Nagulapati <pavan.nagulapati@popcornapps.com>
2022-10-18 15:48:39 +0530

    Updated with employee list oage content

--- pages/component/documentation/crud-api/employeeList.tsx
+++ pages/component/documentation/crud-api/employeeList.tsx
@@ -1,57 +1,62 @@
 import Link from "next/link";
-import axios from 'axios';
+import { useState } from 'react';
+import axios from "axios";
 function EmployeeList({ data }) {
+    /*for (let key in data){
+        console.log(data)
+        for(let i=0;i<data[key].length;i++){
+            if(data[key]!==undefined){
+                console.log(data[key][i].id)
+                console.log(data[key][i].attributes.name)
+            }else{
+
+            }
+        }
+    }*/
+    
     return (
         <div>
-            <div className="container main-container">
-                <Link href="/">
-                    <a>Back To Home</a>
-                </Link>
-                <h5 className="text-center">Employee List</h5>
-                <div className="server-side">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>User Id</th>
-                                <th>Name</th>
-                                <th>Location</th>
-                                <th>Salary</th>
-                                <th>Action</th>
+            <h2 className='topnav'>Rendered By Next JS | Server side rendered</h2>
+            <table className="table">
+                <thead>
+                    <th>Id</th>
+                    <th>User Id</th>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Location</th>
+                    <th>Salary</th>
+                    <th>Action</th>
+                </thead>
+                <tbody>
+                    {data.map(e => {
+                        return (
+                            <tr >
+                                <td>{e.id}</td>
+                                <td>{e.userId}</td>
+                                <td>{e.name}</td>
+                                <td>{e.date}</td>
+                                <td>{e.location}</td>
+                                <td>{e.salary}</td>
+                                <td>
+                                    <button className="btn btn-success">Edit</button>
+                                    <button className="btn btn-danger">Delete</button>
+                                </td>
                             </tr>
-                        </thead>
-                        <tbody>
-                            {data.map(avatar => {
-                                return (
-                                    <tr>
-                                        <td>{avatar.id}</td>
-                                        <td>{avatar.userId}</td>
-                                        <td>{avatar.name}</td>
-                                        <td>{avatar.location}</td>
-                                        <td>{avatar.salary}</td>
-                                        <td>
-                                            <button className="btn btn-warning" >Edit</button>
-                                            <button className="btn btn-danger"  >Delete</button>
-                                            <button className="btn btn-success" >Update</button>
-                                        </td>
-                                    </tr>
-                                )
-                            })}
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div >
     )
 }
 export default EmployeeList;
 export async function getServerSideProps() {
-    // Fetch data from external API
+    // Fetch data from external API 
     const data = await fetch(`https://api-generator.retool.com/eK34lw/data`).then(res => res.json())
-
     // Returning the fetched data
     return { props: { data } }
 }
+
 
 
 
